7ac951296258bcdf414b84d1ed13d4b2d3d33c74
konvolution <35084618+konvolution@users.noreply.github.com>
2021-10-31 15:32:25 +0000

    Improve UI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import * as React from "react";
 import "./styles.css";
 import FlipPage from "react-flip-page";
 
@@ -10,9 +11,29 @@ const pages = [
 ];
 
 export default function App() {
+  const refBook = React.useRef();
+
+  React.useEffect(() => {
+    document.body.addEventListener("keydown", (evt) => {
+      switch (evt.which) {
+        case 37:
+          refBook.current.gotoPreviousPage();
+          break;
+
+        case 39:
+          refBook.current.gotoNextPage();
+          break;
+
+        default:
+          break;
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <FlipPage
+        ref={refBook}
         className="App-Book"
         orientation="horizontal"
         width={800}
@@ -27,6 +48,12 @@ export default function App() {
                 index === 0 ? "title" : `pages ${index * 2 - 1}-${index * 2}`
               }
             />
+            {index === 0 && (
+              <div className="App-Title">
+                <h1>Story by</h1>
+                <h1>Elise Demoore</h1>
+              </div>
+            )}
           </article>
         ))}
         <article className="App-Page-Last" key={pages.length}>
